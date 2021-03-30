38aee6b430784d492c4fc9571901f5b3af1d2fa5
Erez Shomron <erez@mandor>
2021-03-30 19:12:14 +0300

    added some more functions on the TODO

--- main.py
+++ main.py
@@ -39,7 +39,47 @@ def get_all_user_playlists(client, limit = 50):
     return results
 
 
+def get_all_playlist_tracks(client, playlist_id: list, limit = 100):
+    response = client.playlist_tracks(playlist_id, limit = limit)
+    results = response['items']
+    offset = limit
+    while response['next'] is not None:
+        response = client.playlist_tracks(playlist_id, limit = limit,
+                                                  offset = offset)
+        results.extend(response['items'])
+        offset += limit
+    return results
+
+
+def find_playlist(playlist_items: list, playlist_name: str):
+    """
+    Finds a playlist by a given name.
+    :return: tracks' id of the found playlist, otherwise None
+    """
+
+    for plst in playlist_items:
+        if plst['name'] == playlist_name:
+            plst_tracks_id = plst['id']
+            break
+
+    return plst_tracks_id
+
+
+def get_tracks_ids(tracks: list):
+    out = list()
+    for trk in tracks:
+        out.append(trk['track']['id'])
+    return out
+
+
+def get_tracks_audio_features(client, tracks_items: list, limit = 100):
+    ids = get_tracks_ids(tracks_items)
+    response = client.audio_features(ids)
+
+
 spf_client = get_client_object("user-library-read playlist-read-collaborative playlist-read-private")
+user_playlists = get_all_user_playlists(spf_client)
+erez_nadav_plst = find_playlist(user_playlists, "Erez and Nadav")
+plst_tracks = get_all_playlist_tracks(spf_client, erez_nadav_plst)
 
-
-user_playlists = get_all_user_playlists(spf_client, limit = 5)
+x = 1
