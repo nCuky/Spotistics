9345d18c600b6149ed692d016e5c8dc98c88c5ed
Erez Shomron <erez@mandor>
2021-04-05 14:22:04 +0300

    refactor: SpotifyClient class

--- main.py
+++ main.py
@@ -7,12 +7,7 @@ import pandas as pd
 MAX_TRACKS_FOR_FEATURES = 100
 MAX_TRACKS_FOR_PLAYLIST_ITEMS = 100
 AUTH_SCOPE = "user-library-read playlist-read-collaborative playlist-read-private"
-
-
-class SpClient:
-    def __init__(self):
-        pass
-    pass
+REDIRECT_URI = "http://localhost:8888/spotify/callback"
 
 
 def get_token(token_path = None):
@@ -22,40 +17,6 @@ def get_token(token_path = None):
     token_file_text = token_file.readlines()
     token_file.close()
     return token_file_text
-
-
-token = get_token()
-os.environ['SPOTIPY_CLIENT_ID'] = token[0].strip()
-os.environ['SPOTIPY_CLIENT_SECRET'] = token[1].strip()
-os.environ['SPOTIPY_REDIRECT_URI'] = 'http://localhost:8888/spotify/callback'
-
-
-def get_client_object(auth_scope):
-    return sp.Spotify(client_credentials_manager = SpotifyClientCredentials(),
-                      auth_manager = SpotifyOAuth(scope = auth_scope))
-
-
-def get_all_user_playlists(client, limit = 50):
-    response = client.current_user_playlists(limit = limit)
-    results = response['items']
-    offset = limit
-    while response['next'] is not None:
-        response = client.current_user_playlists(limit = limit, offset = offset)
-        results.extend(response['items'])
-        offset += limit
-    return results
-
-
-def get_all_playlist_tracks(client, playlist_id: list, limit = MAX_TRACKS_FOR_PLAYLIST_ITEMS):
-    response = client.playlist_tracks(playlist_id, limit = limit)
-    results = response['items']
-    offset = limit
-    while response['next'] is not None:
-        response = client.playlist_tracks(playlist_id, limit = limit,
-                                                  offset = offset)
-        results.extend(response['items'])
-        offset += limit
-    return results
 
 
 def find_playlist(playlist_items: list, playlist_name: str):
@@ -93,58 +54,117 @@ def get_tracks_names(tracks_items: list):
     return out
 
 
-def get_tracks_audio_features(client, tracks_items: list, limit = MAX_TRACKS_FOR_FEATURES):
-    '''
-    :param client:
-    :param tracks_items:
-    :param limit:
-    :return:
-    '''
-    ids = get_tracks_ids(tracks_items)
-    result = list()
+class SpotifyClient:
+    client = None
 
-    while len(ids) > 0:
-        result.extend(client.audio_features(ids[0:limit]))
+    def __init__(self, token, auth_scope = AUTH_SCOPE,
+                 redirect_uri = REDIRECT_URI):
+        cid = token[0].strip()
+        csecret = token[1].strip()
+        self.auth_manager = SpotifyOAuth(client_id = cid,
+                                         client_secret = csecret,
+                                         redirect_uri = redirect_uri,
+                                         scope = auth_scope)
 
-        # Removing the first {limit} items from the list:
-        ids = ids[limit:]
+    def connect(self):
+        self.client = sp.Spotify(auth_manager = self.auth_manager)
 
-    return result
+    def disconnected(self):
+        self.client = None
+
+    def is_connected(self):
+        if self.client is None:
+            return False
+        return True
+
+    def validate_connection(self):
+        if not self.is_connected():
+            raise ValueError("Client is not connected to Spotify API")
+
+    def get_all_user_playlists(self, limit = 50):
+        self.validate_connection()
+        response = self.client.current_user_playlists(limit = limit)
+        results = response['items']
+        offset = limit
+        while response['next'] is not None:
+            response = self.client.current_user_playlists(limit = limit, offset = offset)
+            results.extend(response['items'])
+            offset += limit
+        return results
+
+    def get_all_playlist_tracks(self, playlist_id: list,
+                                limit = MAX_TRACKS_FOR_PLAYLIST_ITEMS):
+        self.validate_connection()
+        response = self.client.playlist_tracks(playlist_id, limit = limit)
+        results = response['items']
+        offset = limit
+        while response['next'] is not None:
+            response = self.client.playlist_tracks(playlist_id, limit = limit,
+                                                    offset = offset)
+            results.extend(response['items'])
+            offset += limit
+        return results
+
+    def get_tracks_audio_features(self, tracks_items: list,
+                                  limit = MAX_TRACKS_FOR_FEATURES):
+        '''
+        :param tracks_items:
+        :param limit:
+        :return:
+        '''
+        self.validate_connection()
+        ids = get_tracks_ids(tracks_items)
+        result = list()
+
+        while len(ids) > 0:
+            result.extend(self.client.audio_features(ids[0:limit]))
+
+            # Removing the first {limit} items from the list:
+            ids = ids[limit:]
+
+        return result
+
+    def get_specific_audio_feature(self, tracks_items: list,
+                                   audio_feature: str,
+                                   limit = MAX_TRACKS_FOR_FEATURES):
+        '''
+        Returns a list of a desired audio feature for all given tracks.
+        :param tracks_items:
+        :param audio_feature:
+        :param limit:
+        :return:
+        '''
+        self.validate_connection()
+        tracks_audio_features = self.get_tracks_audio_features(tracks_items,
+                                                               limit)
+        result = list()
+
+        for track in tracks_audio_features:
+            # for audio_feature in audio_features
+            result.append(track[audio_feature])
+
+        return result
+
+    def create_tracks_data_frame(self, tracks_items: list,
+                                 audio_features_names: list,
+                                 limit = MAX_TRACKS_FOR_FEATURES):
+        self.validate_connection()
+        tracks_with_features = {'track_name': get_tracks_names(tracks_items)}
+
+        for feat_name in audio_features_names:
+            tracks_with_features[feat_name] = self.get_specific_audio_feature(tracks_items,
+                                                                              audio_feature = feat_name,
+                                                                              limit = limit)
+
+        return pd.DataFrame(data = tracks_with_features)
 
 
-def get_specific_audio_feature(client, tracks_items: list, audio_feature: str, limit = MAX_TRACKS_FOR_FEATURES):
-    '''
-    Returns a list of a desired audio feature for all given tracks.
-    :param client:
-    :param tracks_items:
-    :param audio_feature:
-    :param limit:
-    :return:
-    '''
-    tracks_audio_features = get_tracks_audio_features(client, tracks_items, limit)
-    result = list()
-
-    for track in tracks_audio_features:
-        # for audio_feature in audio_features
-        result.append(track[audio_feature])
-
-    return result
-
-
-def create_tracks_data_frame(client, tracks_items: list, audio_features_names: list, limit = MAX_TRACKS_FOR_FEATURES):
-    tracks_with_features = {'track_name': get_tracks_names(tracks_items)}
-
-    for feat_name in audio_features_names:
-        tracks_with_features[feat_name] = get_specific_audio_feature(client, tracks_items, audio_feature = feat_name, limit = limit)
-
-    return pd.DataFrame(data = tracks_with_features)
-
-
-spf_client = get_client_object("user-library-read playlist-read-collaborative playlist-read-private")
-user_playlists = get_all_user_playlists(spf_client)
-test_plst = find_playlist(user_playlists, "ProgPsy Rock and Neo Psy Rock")
-test_plst_tracks = get_all_playlist_tracks(spf_client, test_plst)
-
-test_df = create_tracks_data_frame(client = spf_client, tracks_items = test_plst_tracks, audio_features_names = ['instrumentalness', 'energy', 'danceability', 'acousticness', 'tempo'])
+my_spotify = SpotifyClient(get_token())
+my_spotify.connect()
+user_playlists = my_spotify.get_all_user_playlists()
+test_plst = find_playlist(user_playlists, "Erez and Nadav")
+test_plst_tracks = my_spotify.get_all_playlist_tracks(test_plst)
+test_df = my_spotify.create_tracks_data_frame(tracks_items = test_plst_tracks,
+                                              audio_features_names = ['instrumentalness', 'energy', 'danceability', 'acousticness', 'tempo'])
 
 x = 1
