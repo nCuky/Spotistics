b9d1385ad27253d005510cd3616e8c618b78db13
nCuky <shvertzen@gmail.com>
2022-11-20 16:58:38 +0200

    commiting main.py before checkout to master

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from logic.app_logic import Logic as lg
 from logic.frontend import plotting as plt
 
 # Initializing the application:
-my_lg = lg(listen_history_from = 'json')
+my_lg = lg(listen_history_from = 'db')
 
 # plt.top_artists_by_listen_count(my_lg)
 # plt.top_artists_by_total_listen_time(my_lg)
